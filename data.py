2869c8da7a547b080680607ed766c80d68814e65
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-04-23 20:32:42 +0500

    Initial commit

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+        {
+            id: 1,
+            'title': 'Article One',
+            'body': 'Detected change in G:\\Flask\\myflaskapp\\App.py, reloading',
+            'author': 'Adnan Shah',
+            'date_created': '23 April 2020'
+        },
+        {
+            id: 2,
+            'title': 'Article Two',
+            'body': 'Detected change in G:\\Flask\\myflaskapp\\App.py, reloading',
+            'author': 'Rizwan Shah',
+            'date_created': '23 April 2020'
+        },
+        {
+            id: 1,
+            'title': 'Article Three',
+            'body': 'Detected change in G:\\Flask\\myflaskapp\\App.py, reloading',
+            'author': 'Anzil Shah',
+            'date_created': '23 April 2020'
+        }
+    ]
+    return articles
+
+
+
